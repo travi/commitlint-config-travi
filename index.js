46092d35bb2c415d8cc80cbff566b9bc32835687
Matt Travi <programmer@travi.org>
2017-09-05 22:21:34 -0500

    feat(wip): add the ability to use wip
    sometimes commits do not complete a feat or fix and dont fit the other categories because it is work toward a feat or fix , so wip helps to signify this

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = {extends: ['@commitlint/config-angular']};
+const config = require('@commitlint/config-angular');
+
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        ...config.rules['type-enum'][2],
+        'wip'
+      ]
+    ]
+  }
+};
