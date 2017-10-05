f1d9ea34b0dd0ba37e442dd5041ed62e8590a329
Matt Travi <programmer@travi.org>
2017-10-04 21:47:30 -0500

    feat(scope-case): defined the casing rule for the scope

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('@commitlint/config-angular');
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
+    'scope-case': [2, 'always', 'kebab-case'],
     'type-enum': [
       2,
       'always',
