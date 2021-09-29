74b81b7a304a6f95a318b1263e63dd4b160d4320
Matt Travi <programmer@travi.org>
2021-09-28 22:15:38 -0500

    fix: actually extended the form8ion config

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const config = require('@commitlint/config-conventional');
-
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-case': [2, 'always', 'kebab-case'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        ...config.rules['type-enum'][2],
-        'wip'
-      ]
-    ]
-  }
+  extends: ['@form8ion']
 };
